8f920a40d284ef4b4261fb29be75c9fb0317842b
Dustin Michels <dustin7538@gmail.com>
2019-06-28 18:29:08 -0600

    more experimentation

--- paradeller/analysis.py
+++ paradeller/analysis.py
@@ -1,5 +1,9 @@
-import emoji
 import string
+from collections import defaultdict
+from itertools import combinations
+
+import emoji
+from tqdm import tqdm, trange
 
 
 def tokenize(text, keep_emoji=False):
@@ -17,8 +21,10 @@ def tokenize(text, keep_emoji=False):
     list
         list of standardized tokens
     """
-    # remove punctuaation
-    text = text.translate(str.maketrans("", "", string.punctuation))
+    # replace punctuation with whitespace
+    text = text.translate(
+        str.maketrans(string.punctuation, " " * len(string.punctuation))
+    )
 
     # handle emoji characters
     emoji_pattern = emoji.get_emoji_regexp()
@@ -32,3 +38,43 @@ def tokenize(text, keep_emoji=False):
     # split into words & clean each word
     words = text.split()
     return [w.lower().strip() for w in words]
+
+
+def create_word_dict(data):
+    d = defaultdict(list)
+    for item in data:
+        # get tokens
+        tokens = tokenize(item["text"])
+        for token in tokens:
+            d[token].append(item["id"])
+    return dict(d)
+
+
+def get_combos(tokens):
+    """
+    Keep all combinations of token (sorted)
+    """
+    total = []
+    for i in range(1, len(tokens) + 1):
+        combos = list(combinations(sorted(tokens), i))
+        total.extend(combos)
+    return total
+
+
+def load_all(data: list):
+    """
+    Create dict where combo-tuples are keys
+    and list of ids are values
+    """
+    my_data = defaultdict(list)
+
+    # for item in tqdm(data):
+    for item in data:
+        # tokenize
+        tokens = tokenize(item["text"])
+        # find comobos
+        combos = get_combos(tokens)
+        # add to record
+        for combo in combos:
+            my_data[combo].append(item["id"])
+    return my_data
