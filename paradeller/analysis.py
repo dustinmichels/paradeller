56e8c80d1d69c9dbab24939dd819c87b44208bc8
Dustin Michels <dustin7538@gmail.com>
2019-07-09 00:36:55 -0600

    can search for complete poems

--- paradeller/analysis.py
+++ paradeller/analysis.py
@@ -1,6 +1,7 @@
 import string
 from collections import defaultdict
-from itertools import combinations, chain
+from itertools import chain, combinations
+from math import factorial as fact
 from typing import Dict, Iterable, List, Set
 
 import emoji
@@ -12,11 +13,11 @@ from tqdm.auto import tqdm, trange
 
 def find_matches_for_start_pairs(pairs, adj_list_ids, adj_list_words):
     """Given a collection of potential start pairs, look for matches on each"""
-    all_valid = {}
+    all_valid = []
     for p in tqdm(pairs):
         valid = find_matches(p[0], p[1], adj_list_ids, adj_list_words)
         if valid:
-            all_valid[p] = valid
+            all_valid.append((p, valid))
     return all_valid
 
 
@@ -66,57 +67,6 @@ def find_valid_matches(pot_ids, adj_list_ids, stanza_words):
 # ---------- FIND POEMS ----------
 
 
-def find_final_stanzas(stan1, stan2, stan3, adj_list_ids, adj_list_words, all_valid):
-
-    pair1, pair2, pair3 = stan1[:2], stan2[:2], stan3[:2]
-
-    # combine words from tweets
-    prev_stanza_words = list(
-        chain.from_iterable([adj_list_ids[line] for line in [*pair1, *pair2, *pair3]])
-    )
-
-    # look for other tweets with those words
-    pot_ids = get_potential_final_lines(prev_stanza_words, adj_list_words)
-
-    # --- filter down ---
-    # remove lines from previous stanzas
-    # ensure tweets contain subset of master_word_set
-    pot_ids = pot_ids - {*stan1, *stan2, *stan3}
-    pot_ids = [x for x in pot_ids if set(adj_list_ids[x]) <= set(prev_stanza_words)]
-
-    # look for valid pairs of potential tweets
-    if len(pot_ids) > 1:
-        return find_valid_matches(pot_ids, adj_list_ids, stanza_words)
-    return []
-
-
-def get_potential_final_lines(stanza_words, adj_list_words) -> Set[int]:
-    """Potential lines for final stanza"""
-    potential_ids: Set[int] = set()
-    for word in stanza_words:
-        potential_ids.update(adj_list_words[word])
-    return potential_ids
-
-
-def find_valid_lines(pot_ids, adj_list_ids, stanza_words):
-    """Find valid lines for final stanza"""
-
-    # make pairings of two potential tweets
-    combos = list(combinations(pot_ids, 2))
-
-    # for each pair:
-    # get combination of words, check if equals stanza_words
-    valid = []
-    for pair in combos:
-        words = adj_list_ids[pair[0]] + adj_list_ids[pair[1]]
-        if sorted(words) == sorted(stanza_words):
-            valid.append(pair)
-    return valid
-
-
-# ---------- READ RESULTS ----------
-
-
 def get_stanzas(all_valid):
     stanzas = []
     for pair, matches in all_valid:
@@ -127,3 +77,59 @@ def get_stanzas(all_valid):
             stanza = stanza_start + [a, b]
         stanzas.append(tuple(stanza))
     return stanzas
+
+
+def find_final_stanzas_from_stanzas(stanzas, adj_list_ids, adj_list_words):
+    """Given a collection of potential start pairs, look for matches on each"""
+
+    # create combos generator
+    all_combos = combinations(stanzas, 3)
+
+    # filtered generator (all lines unique)
+    combos = (c for c in all_combos if len(set().union(*c)) == 12)
+
+    all_valid = {}
+    for combo in tqdm(combos):
+        valid = find_final_stanzas(*combo, adj_list_ids, adj_list_words)
+        if valid:
+            all_valid[combo] = valid
+    return all_valid
+
+
+def find_final_stanzas(stan1, stan2, stan3, adj_list_ids, adj_list_words):
+    pair1, pair2, pair3 = stan1[:2], stan2[:2], stan3[:2]
+
+    # combine words from tweets
+    prev_stanza_words = list(
+        chain.from_iterable([adj_list_ids[line] for line in [*pair1, *pair2, *pair3]])
+    )
+
+    # look for other tweets with those words
+    pot_ids = get_potential_tweets(prev_stanza_words, adj_list_words)
+
+    # --- filter down ---
+    # remove lines from previous stanzas
+    # ensure tweets contain subset of master_word_set
+    pot_ids = pot_ids - {*stan1, *stan2, *stan3}
+    pot_ids = [x for x in pot_ids if set(adj_list_ids[x]) <= set(prev_stanza_words)]
+
+    # look for valid pairs of potential tweets
+    if len(pot_ids) > 1:
+        return find_valid_final_lines(pot_ids, adj_list_ids, prev_stanza_words)
+    return []
+
+
+def find_valid_final_lines(pot_ids, adj_list_ids, prev_stanza_words):
+    """Find valid lines for final stanza"""
+
+    # make pairings of two potential tweets
+    combos = list(combinations(pot_ids, 6))
+
+    # for each pair:
+    # get combination of words, check if equals stanza_words
+    valid = []
+    for stanza in combos:
+        words = list(chain.from_iterable([adj_list_ids[line] for line in stanza]))
+        if sorted(words) == sorted(prev_stanza_words):
+            valid.append(stanza)
+    return valid
--- paradeller/run.py
+++ paradeller/run.py
@@ -8,7 +8,12 @@ from statistics import mean
 
 from tqdm.auto import tqdm
 
-from paradeller.analysis import find_matches, find_matches_for_start_pairs
+from paradeller.analysis import (
+    find_matches,
+    find_matches_for_start_pairs,
+    get_stanzas,
+    find_final_stanzas,
+)
 from paradeller.helper import read_from_pickle
 
 # load saved data from pickle
@@ -22,9 +27,15 @@ for tweet_id, words in tqdm(adj_list_ids.items()):
     pop.append((tweet_id, mean([len(adj_list_words[word]) for word in words])))
 pop.sort(key=lambda x: x[1], reverse=True)
 
-# define helper function that takes single parameter
+# define helper functions that takes single parameter
+
+
 def find_matches_for_pair(p):
     return find_matches(p[0], p[1], adj_list_ids, adj_list_words)
+
+
+def find_final_stanzas_helper(stanzas):
+    find_final_stanzas(*stanzas, adj_list_ids, adj_list_words)
 
 
 if __name__ == "__main__":
@@ -33,6 +44,8 @@ if __name__ == "__main__":
     # parse command line arguments
     args_dict = dict(enumerate(sys.argv))
     n = int(args_dict.get(1, default_n))
+
+    # ---------- LOOK FOR STANZAS ----------
 
     # choose some IDS
     some_ids = [x[0] for x in pop[:n]]
@@ -49,10 +62,37 @@ if __name__ == "__main__":
 
     # get filename
     d = datetime.utcnow()
-    filename = "data/found_{}.pickle".format(d.strftime("%Y-%m-%d-%H-%M"))
+    filename = "data/found_stanzas_{}.pickle".format(d.strftime("%Y-%m-%d-%H-%M"))
 
     # save to file
     with open(filename, "wb") as f:
         pickle.dump(all_valid, f)
+    print("stanzas saved to data/found_stanzas_[datetime].pickle")
 
-    print("results saved to data/found_[datetime].pickle")
+    # ---------- LOOK FOR PARADELLES ----------
+    if len(all_valid) >= 3:
+        stanzas = get_stanzas(all_valid)
+
+        # find combos to chcek
+        print("Finding combos to check...")
+        all_combos = combinations(stanzas, 3)
+        combos = [c for c in all_combos if len(set().union(*c)) == 12]
+
+        # look for complete paradelles
+        print("searching for complete paradelles within found stanzas")
+        with Pool(os.cpu_count()) as pool:
+            res = list(
+                tqdm(pool.imap(find_final_stanzas_helper, combos), total=len(combos))
+            )
+        all_poems = [x for x in list(zip(combos, res)) if x[1]]
+        print("Found {} poems.".format(len(all_poems)))
+
+        # get filename
+        filename = "data/found_poems_{}.pickle".format(d.strftime("%Y-%m-%d-%H-%M"))
+
+        # save to file
+        with open(filename, "wb") as f:
+            pickle.dump(all_valid, f)
+        print("results saved to data/found_poems_[datetime].pickle")
+    else:
+        print("No complete poems found :(")
