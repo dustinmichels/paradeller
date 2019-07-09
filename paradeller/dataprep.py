9a5377df85e1712c08e36c94f0946b6c83159fb6
Dustin Michels <dustin7538@gmail.com>
2019-07-09 14:39:49 -0600

    refactor code, add to notebooks

--- paradeller/dataprep.py
+++ paradeller/dataprep.py
@@ -1,5 +1,6 @@
 import string
 from collections import defaultdict
+from statistics import mean
 from typing import Dict, Iterable, List
 
 import emoji
@@ -9,15 +10,9 @@ from tqdm.auto import tqdm, trange
 from paradeller.helper import load_archive, read_from_pickle, save_to_pickle
 
 
-def tokenize(text, keep_emoji=False):
+def tokenize(text):
     """
     Tokenize tweet text (split into list cleaned-up words)
-
-    Parameters
-    ----------
-    keep_emoji : bool
-        Whether emojis should be retained like words
-        or stripped away like punctuation
 
     Returns
     -------
@@ -25,15 +20,11 @@ def tokenize(text, keep_emoji=False):
         list of standardized tokens
     """
     # replace punctuation with whitespace
-    text = text.translate(
-        str.maketrans(string.punctuation, " " * len(string.punctuation))
-    )
-    # either add space between emoji chars or remove
+    punc = string.punctuation + "“" + "”"
+    text = text.translate(str.maketrans(punc, " " * len(punc)))
+    # remove emoji
     emoji_pattern = emoji.get_emoji_regexp()
-    if keep_emoji:
-        text = " ".join(emoji_pattern.split(text))
-    else:
-        text = emoji_pattern.sub("", text)
+    text = emoji_pattern.sub("", text)
     # split into words & clean each word
     words = text.split()
     return [w.lower().strip() for w in words]
@@ -42,13 +33,14 @@ def tokenize(text, keep_emoji=False):
 # ---------- FILTERING ----------
 
 
-def find_duplicates(data):
+def find_duplicates(data, **kwargs):
     """
     Keys are unique word sets (sorted tuples),
     values are list of ids
     """
+    nobar = kwargs.get("nobar", False)
     duplicates = defaultdict(list)
-    for item in tqdm(data):
+    for item in tqdm(data, disable=nobar):
         words = tokenize(item["text"])
         words_tup = tuple(sorted(words))
         duplicates[words_tup].append(item["id"])
@@ -64,44 +56,43 @@ def filter_out_duplicates(data, duplicates):
     return [x for x in data if x["id"] in unique_phrase_ids]
 
 
-def filter_out_short(data, n=3):
+def filter_out_short(data, n=3, **kwargs):
     """Only keep tweets with # tokens >= n"""
-    return [x for x in tqdm(data) if len(tokenize(x["text"])) >= n]
+    nobar = kwargs.get("nobar", False)
+    return [x for x in tqdm(data, disable=nobar) if len(tokenize(x["text"])) >= n]
 
 
-def filter_out_oddballs(data):
+def filter_out_oddballs(data, **kwargs):
     """
     Filter out tweets that don't share each word with 2 other lines
     """
     # create an adj list by word
-    adj_list_words = create_adj_list_by_word(data)
-
+    adj_list_words = create_adj_list_by_word(data, **kwargs)
     # find words with too few matches
     filtered_adj_list_words = {k: v for k, v in adj_list_words.items() if len(v) < 3}
-
     # consolidate ids to remove
     rm_ids = set().union(*filtered_adj_list_words.values())
-
     # remove from data
     return [x for x in data if x["id"] not in rm_ids]
 
 
-def filter_out_oddballs_recursive(data):
+def filter_out_oddballs_recursive(data, verbose=True, **kwargs):
+    printif = lambda s: print(s) if verbose else None
     start_len = len(data)
-    data = filter_out_oddballs(data)
+    data = filter_out_oddballs(data, **kwargs)
     diff = start_len - len(data)
     if diff == 0:
-        print("Nothing removed. Done filtering.")
+        printif("Nothing removed. Done filtering.")
         return data
     else:
-        print(f"{diff:,} tweets removed. Running again.")
-        return filter_out_oddballs_recursive(data)
+        printif(f"{diff:,} tweets removed. Running again.")
+        return filter_out_oddballs_recursive(data, verbose=verbose, **kwargs)
 
 
 # ---------- RESTRUCTURING ----------
 
 
-def restructure_data(data) -> tuple:
+def restructure_data(data, **kwargs) -> tuple:
     """
     Restructure data into 2 adjacency lists.
 
@@ -116,10 +107,12 @@ def restructure_data(data) -> tuple:
         adj_list_by_word -- {word: {ids}}
         adj_list_by_id -- {id  : [words]}
     """
+    nobar = kwargs.get("nobar", False)
+
     adj_list_by_word: Dict[str, set] = defaultdict(set)
     adj_list_by_id: Dict[int, set] = defaultdict(set)
 
-    for item in tqdm(data):
+    for item in tqdm(data, disable=nobar):
         tokens = tokenize(item["text"])
         adj_list_by_id[item["id"]] = tokens
         for token in tokens:
@@ -127,24 +120,75 @@ def restructure_data(data) -> tuple:
     return dict(adj_list_by_word), dict(adj_list_by_id)
 
 
-def create_adj_list_by_id(data):
+def create_adj_list_by_id(data, **kwargs):
+    nobar = kwargs.get("nobar", False)
     adj_list_by_id: Dict[int, set] = defaultdict(set)
-    for item in tqdm(data):
+    for item in tqdm(data, disable=nobar):
         tokens = tokenize(item["text"])
         adj_list_by_id[item["id"]] = tokens
     return dict(adj_list_by_id)
 
 
-def create_adj_list_by_word(data):
+def create_adj_list_by_word(data, **kwargs):
+    nobar = kwargs.get("nobar", False)
     adj_list_by_word: Dict[str, set] = defaultdict(set)
-    for item in tqdm(data):
+    for item in tqdm(data, disable=nobar):
         tokens = tokenize(item["text"])
         for token in tokens:
             adj_list_by_word[token].add(item["id"])
     return dict(adj_list_by_word)
 
 
+def create_duplicates_dict(adj_list_ids, duplicates):
+    """Restructure initial duplicates dictionary into a neater one"""
+    d = {}
+    for id_, words in adj_list_ids.items():
+        sorted_words = tuple(sorted(words))
+        dups = set(duplicates[sorted_words]) - {id_}
+        if dups:
+            d[id_] = list(dups)
+    return d
+
+
 # ---------- COMBINED ----------
+
+
+def prep_data(data, verbose=True):
+    """
+    Returns tuple: data, duplicates, adj_list_words, adj_list_ids
+    """
+    showlen = lambda data: print(f"Length: {len(data):,}") if verbose else None
+    printif = lambda s: print(s) if verbose else None
+    nobar = not verbose
+
+    showlen(data)
+    printif("\nCleaning up data...")
+
+    # remove too short
+    printif("> Remove too short") if verbose else None
+    data = filter_out_short(data, nobar=nobar)
+    showlen(data)
+
+    # remove duplicate phrases
+    printif("> Remove duplicate phrases")
+    duplicates = find_duplicates(data, nobar=nobar)
+    data = filter_out_duplicates(data, duplicates)
+    showlen(data)
+
+    # remove oddballs (too few matches)
+    printif("> Recursively remove oddballs")
+    data = filter_out_oddballs_recursive(data, nobar=nobar, verbose=verbose)
+    showlen(data)
+
+    # make adj lists
+    printif("\nCreating adjacency lists...")
+    adj_list_words, adj_list_ids = restructure_data(data, nobar=nobar)
+
+    # restructure duplicates
+    printif("\nRestructing duplicates...")
+    duplicates = create_duplicates_dict(adj_list_ids, duplicates)
+
+    return data, duplicates, adj_list_words, adj_list_ids
 
 
 def load_and_prep(use_pickle=False, update_pickle=False):
@@ -161,31 +205,7 @@ def load_and_prep(use_pickle=False, update_pickle=False):
     else:
         print("Loading unprocessed real data...")
         data = load_archive()
-
-        showlen = lambda data: print(f"Length: {len(data):,}")
-
-        showlen(data)
-        print("\nCleaning up data...")
-
-        # remove too short
-        print("> Remove too short")
-        data = filter_out_short(data)
-        showlen(data)
-
-        # remove duplicate phrases
-        print("> Remove duplicate phrases")
-        duplicates = find_duplicates(data)
-        data = filter_out_duplicates(data, duplicates)
-        showlen(data)
-
-        # remove oddballs (too few matches)
-        print("> Recursively remove oddballs")
-        data = filter_out_oddballs_recursive(data)
-        showlen(data)
-
-        print("\nCreating adjacency lists...")
-        # make adj lists
-        adj_list_words, adj_list_ids = restructure_data(data)
+        data, duplicates, adj_list_words, adj_list_ids = prep_data(data)
 
         if update_pickle:
             print("\nSaving new data to pickle...")
@@ -203,3 +223,15 @@ def load_and_prep(use_pickle=False, update_pickle=False):
         print(f"{k:15} type: {type(v)}\tlen: {len(v):,}")
 
     return data, duplicates, adj_list_words, adj_list_ids
+
+
+# --------- SORT ----------
+
+
+def sort_ids_by_popularity(adj_list_ids, adj_list_words):
+    pop = []
+    for id_, words in tqdm(adj_list_ids.items()):
+        pop.append((id_, mean([len(adj_list_words[word]) for word in words])))
+    pop.sort(key=lambda x: x[1], reverse=True)
+    ids = [x[0] for x in pop]
+    return ids
