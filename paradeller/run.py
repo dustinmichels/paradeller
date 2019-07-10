75eaefb2d3ae8997d9410eac548c6eb8609864fe
Dustin Michels <dustin7538@gmail.com>
2019-07-10 10:35:08 -0600

    add google profiler?

--- paradeller/run.py
+++ paradeller/run.py
@@ -5,6 +5,7 @@ from multiprocessing import Pool
 
 from tqdm.auto import tqdm
 
+import googlecloudprofiler
 from paradeller.analysis import (
     consolidate_poems,
     consolidate_stanzas,
@@ -25,11 +26,31 @@ def find_final_stanzas_helper(stanzas):
     return find_final_stanzas(*stanzas, adj_list_ids, adj_list_words)
 
 
+def init_google_profiler():
+    # Profiler initialization. It starts a daemon thread which continuously
+    # collects and uploads profiles. Best done as early as possible.
+    try:
+        googlecloudprofiler.start(
+            service="hello-profiler",
+            service_version="1.0.1",
+            # verbose is the logging level. 0-error, 1-warning, 2-info,
+            # 3-debug. It defaults to 0 (error) if not set.
+            verbose=3,
+            # project_id must be set if not running on GCP.
+            # project_id='my-project-id',
+        )
+    except (ValueError, NotImplementedError) as exc:
+        print(exc)  # Handle errors here
+
+
 if __name__ == "__main__":
+    # init profiling
+    init_google_profiler()
+
+    # parse command line arguments
     default_n = "100"
     default_style = "load"  # or "load" | "fresh"
 
-    # parse command line arguments
     args_dict = dict(enumerate(sys.argv))
     n = int(args_dict.get(1, default_n))
     style = args_dict.get(2, default_style)
@@ -65,14 +86,12 @@ if __name__ == "__main__":
         # find combos to chcek
         print("Finding combos to check (this may take a while)...")
         all_combos = combinations(stanzas, 3)
-        combos = [c for c in all_combos if len(set().union(*c)) == 12]
+        combos = (c for c in all_combos if len(set().union(*c)) == 12)
 
         # look for complete paradelles
         print("Searching for complete paradelles")
         with Pool(os.cpu_count()) as pool:
-            res = list(
-                tqdm(pool.imap(find_final_stanzas_helper, combos), total=len(combos))
-            )
+            res = list(tqdm(pool.imap(find_final_stanzas_helper, combos)))
         valid_poems = [x for x in list(zip(combos, res)) if x[1]]
         poems = consolidate_poems(valid_poems)
         print(f"Found {len(poems)} poems.")
